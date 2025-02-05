4348aa5d79f5e20c81bdad7711b336e263e312c6
jonortega <jon.ortega.g@gmail.com>
2025-02-05 17:51:55 +0100

    Corregido anchura del panel del userProfile para ajustarse siempre

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -29,7 +29,7 @@ export default async function UserProfile() {
   }
 
   return (
-    <div className='flex items-center justify-center space-x-4 bg-spotify-gray-300 p-6 rounded-lg border-2 border-spotify-gray-200 max-w-sm mx-auto shadow-lg'>
+    <div className='flex items-center justify-center space-x-4 bg-spotify-gray-300 p-6 rounded-lg border-2 border-spotify-gray-200 max-w-fit mx-auto shadow-lg'>
       {user.imageUrl ? (
         <Image src={user.imageUrl} alt={user.name} width={80} height={80} className='rounded-full  mr-2' />
       ) : (
